cb6d5c20f0ce47cfa183b915e1e3b8b94e7e0575
teiron3 <36124310+teiron3@users.noreply.github.com>
2024-12-06 06:13:51 +0900

    Add files via upload

---
+++ ja_ja.ps1
@@ -0,0 +1,5 @@
+﻿Set-WinUserLanguageList -Force ja-JP      # 言語リストとプロパティを日本語に設定
+Set-WinSystemLocale -SystemLocale ja-JP   # システムロケールを 日本 に変更
+Set-WinUILanguageOverride -Language ja-JP # 表示言語と地域設定を 日本語 に変更
+Set-WinHomeLocation 122                   # 国と地域を 日本 に変更
+Restart-Computer
